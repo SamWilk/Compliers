030ae4f0a45f45cd5f8d725eba8d886f132c9469
SamWilk <samwilk18@yahoo.com>
2022-02-05 18:52:58 -0500

    More updates to Little file

--- .antlr/LittleParser.java
+++ .antlr/LittleParser.java
@@ -17,7 +17,7 @@ public class LittleParser extends Parser {
 		new PredictionContextCache();
 	public static final int
 		T__0=1, WS=2, KEYWORD=3, OPERATOR=4, IDENTIFIER=5, STRINGLITERAL=6, INTLITERAL=7, 
-		FLOATLITERAL=8, Comment=9;
+		Comment=8;
 	public static final int
 		RULE_r = 0;
 	private static String[] makeRuleNames() {
@@ -36,7 +36,7 @@ public class LittleParser extends Parser {
 	private static String[] makeSymbolicNames() {
 		return new String[] {
 			null, null, "WS", "KEYWORD", "OPERATOR", "IDENTIFIER", "STRINGLITERAL", 
-			"INTLITERAL", "FLOATLITERAL", "Comment"
+			"INTLITERAL", "Comment"
 		};
 	}
 	private static final String[] _SYMBOLIC_NAMES = makeSymbolicNames();
@@ -119,8 +119,8 @@ public class LittleParser extends Parser {
 	}
 
 	public static final String _serializedATN =
-		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\13\7\4\2\t\2\3\2"+
-		"\3\2\3\2\2\2\3\2\2\2\2\5\2\4\3\2\2\2\4\5\7\3\2\2\5\3\3\2\2\2\2";
+		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\n\7\4\2\t\2\3\2\3"+
+		"\2\3\2\2\2\3\2\2\2\2\5\2\4\3\2\2\2\4\5\7\3\2\2\5\3\3\2\2\2\2";
 	public static final ATN _ATN =
 		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
 	static {
--- Little.g4
+++ Little.g4
@@ -10,13 +10,16 @@ KEYWORD: ('PROGRAM'|'BEGIN'|'STRING'|'FUNCTION'|'INT'|'IF'|'ELSE'|'ENDIF'|'END'|
 OPERATOR: (':='|';'|'('|')'|'<'|'>'|'='|'!='|'-'|'+'|','|'*'|'/'|
 '<='|'>=');
 
-IDENTIFIER: ([A-Za-z0-9]+) ; // Still need to change this so numbers are not caught
+//Might work, just have to test it
+IDENTIFIER: (~([0-9])[A-Za-z0-9]+) ;
 
 STRINGLITERAL: ('"'~('"')*'"');
 
 INTLITERAL: [0-9]+ ;
 
-FLOATLITERAL: [0-9]('.'[0-9]+)? ;
+//I don't know exactly what a fragment does, but made it so, since having [0-9]* says it might be am empty string and is throwing an error
+fragment FLOATLITERAL: (([0-9]*)('.'[0-9]+)?) ;
 
 Comment: '--'~('\r' | '\n')* -> skip;
+
 
