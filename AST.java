501bcecf36c1c2ad8f28039931c1a36785b674ba
SamWilk <samwilk18@yahoo.com>
2022-04-21 14:36:00 -0400

    Have it grabbing variables correctly and printing them out correctly

---
+++ AST.java
@@ -0,0 +1,31 @@
+// class AST {
+
+//     public class Node{
+
+//         public String current;
+//         public String type;
+//         public String code;
+//         public Node left;
+//         public Node right;
+
+//         public Node(String rootNode){
+//             this.current = rootNode;
+//             this.type = ":=";
+//             this.code = "STORE";
+//         }
+
+//     }
+
+//     public Node root;
+
+//     public AST(String rootNode){
+//         Node node = new Node(rootNode);
+//         this.root = node;
+//     }
+
+//     public String getRoot(){
+//         return this.root;
+//     }
+
+
+// }
